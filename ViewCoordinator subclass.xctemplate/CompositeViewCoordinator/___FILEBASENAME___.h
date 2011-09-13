422e5c5b7429809a25f8fb3699a15351cf29c084
Luke Cunningham <luke.cunningham@rea-group.com>
2011-09-13 17:27:14 +1000

    added ViewCoordinator & CompositeViewCoordinator templates

---
+++ ViewCoordinator subclass.xctemplate/CompositeViewCoordinator/___FILEBASENAME___.h
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "CompositeViewCoordinator.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : CompositeViewCoordinator
+
+@end
---
+++ ViewCoordinator subclass.xctemplate/CompositeViewCoordinator/___FILEBASENAME___.m
@@ -0,0 +1,70 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Properties
+
+// synthesize your properties here
+
+
+- (id)initWithReferenceController:(UIViewController *)controller;
+{
+  if ((self = [super initWithReferenceController:controller]))
+	{
+    // Initialization code
+  }
+
+  return self;
+}
+
+- (void)dealloc;
+{
+	// release your properties here
+	
+	[super dealloc];
+}
+
+
+#pragma mark - ___FILEBASENAMEASIDENTIFIER___
+
+// your classes methods here
+
+
+#pragma mark - View lifecycle
+
+/*
+- (void)loadView;
+{
+	// don't call super if you don't want a default view created
+  [super loadView];
+
+	// setup your view programmatically here
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad;
+{
+  [super viewDidLoad];
+}
+*/
+
+/*
+- (void)viewDidUnload;
+{
+  [super viewDidUnload];
+  // Release any retained subviews of the main view.
+  // e.g. self.myOutlet = nil;
+}
+*/
+
+@end
